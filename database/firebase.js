109b8b4648cbe08fe2c945f403021f545c3d7909
PabloMesquida <pablomesquida67@gmail.com>
2022-11-17 02:32:59 +0100

    firebase-config

--- database/firebase.js
+++ database/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase/compat";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDXhWF9KH4Sn69YGSuXfUdTGjgYe_D4P9A",
-  authDomain: "react-n-firebase-2d833.firebaseapp.com",
-  projectId: "react-n-firebase-2d833",
-  storageBucket: "react-n-firebase-2d833.appspot.com",
-  messagingSenderId: "187672745300",
-  appId: "1:187672745300:web:781fcc1f2631822581a4c1",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const app = firebase.initializeApp(firebaseConfig);
